60b8723dc5960358a14de5d1ac3dd77cd0396537
Welsen <welsen9595@gmail.com>
2023-01-20 11:17:19 +0800

    Add test case

--- Java/test/GroupMediumTests.java
+++ Java/test/GroupMediumTests.java
@@ -1,13 +1,22 @@
 import Java.medium.Medium_227_BasicCalculator_II;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class GroupMediumTests {
 
     @Test
     public void Medium_227_BasicCalculator_II_Test() {
+
+        String test_case_1 = "3+2*2";
+        String test_case_2 = "3+2*2+20";
+
         Medium_227_BasicCalculator_II test = new Medium_227_BasicCalculator_II();
-        int result_1 = test.simpleCalculate("3+2*2");
-        System.out.println(result_1);
+        int result_1 = test.simpleCalculate(test_case_1);
+        int result_2 = test.simpleCalculate(test_case_2);
+
+        Assert.assertEquals(result_1, 7);
+        Assert.assertEquals(result_2, 27);
+
     }
 
 
