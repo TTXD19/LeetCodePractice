a4dd1207e155e0688c5628d4aeb798bc6b98deba
Welsen <welsen9595@gmail.com>
2023-01-20 11:10:02 +0800

    Add new questions

---
+++ Java/test/GroupMediumTests.java
@@ -0,0 +1,14 @@
+import Java.medium.Medium_227_BasicCalculator_II;
+import org.junit.Test;
+
+public class GroupMediumTests {
+
+    @Test
+    public void Medium_227_BasicCalculator_II_Test() {
+        Medium_227_BasicCalculator_II test = new Medium_227_BasicCalculator_II();
+        int result_1 = test.simpleCalculate("3+2*2");
+        System.out.println(result_1);
+    }
+
+
+}
